3157f80750e6fe33c44eb6611881af76a19fbcde
Sauvic <sauvicpaulchy@gmail.com>
2022-09-27 01:07:19 +0530

    Created navigation and home components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/Home/Home";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <Home />
+      </main>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import LeetCode from "../../assests/leetcode.svg";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/sauvicpaulchy/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://github.com/Sauvic016"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github"></i>
+      </a>
+      <a
+        href="https://leetcode.com/Sauvic_PC/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <img
+          src={LeetCode}
+          alt="abc"
+          style={{ height: "24px", width: "24px" }}
+        />
+      </a>
+    </div>
+  );
+};
+
+export default Social;
